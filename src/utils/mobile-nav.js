f4af0f0596f623799398f43abfa05f0bb2804f26
SantiagoVillagra <sanmavillagra@gmail.com>
2025-04-04 20:26:36 -0300

    probando trabajo en ramas

--- src/utils/mobile-nav.js
+++ src/utils/mobile-nav.js
@@ -1,7 +1,7 @@
 const mobileNav = () => {
   const headerBtn = document.querySelector(".header__bars");
   const mobileNav = document.querySelector(".mobile-nav");
-  const mobileLinks = document.querySelectorAll("mobile-nav__link");
+  const mobileLinks = document.querySelectorAll(".mobile-nav__link");
 
   let isMobileNavOpen = false;
 
@@ -18,6 +18,7 @@ const mobileNav = () => {
 
   mobileLinks.forEach((link) => {
     link.addEventListener("click", () => {
+      console.log('evento')
         isMobileNavOpen = false
         mobileNav.style.display = 'none'
         document.body.style.overflowY = 'auto'
